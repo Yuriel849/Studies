5d77d5130f21fb3950f2d8e8041611269efc6c24
Yuriel849 <yuriel849sp@gmail.com>
2018-11-06 22:11:12 +0900

    create on 6 Nov. 2018

---
+++ 007 -- 06.NOV.2018 (OOP)/src/study/OOP_Game.java
@@ -0,0 +1,16 @@
+package study;
+
+// class variable - instance variable exercise
+class Marine { // 병사
+	static int maxHP = 100; // 병사의 최대 체력 --> 공통
+	int hp; // 개별 병사의 체력 --> 개별
+	static int power = 6; // 병사의 공격력 --> 공통
+	
+	void attack() { } // 병사의 공격하는 기능
+	void move() { }
+	void stop() { } 
+}
+
+public class OOP_Game {
+
+}
---
+++ 007 -- 06.NOV.2018 (OOP)/src/study/OOP_TvTest.java
@@ -0,0 +1,41 @@
+package study;
+
+// 1. 클래스 정의
+class Tv { // Tv 클래스의 시작
+	String color; // Tv의 색깔
+	boolean power; // 전원 on/off --> true면 on, false면 off
+	int channel; // 채널
+	
+	void power() {
+		power = !power; // 전원 on/off 전환하기
+		// 위 한 줄과 같은 if-else문
+//		if(power==true)
+//			power = false;
+//		else
+//			power = true;
+	}
+	void channelUp() {
+		channel++; // 채널 올리기
+	}
+	void channelDown() {
+		channel--; // 채널 내리기
+	}
+} // Tv 클래스의 끝
+
+public class OOP_TvTest {
+	public static void main(String[] args) {
+		// 2. 객체의 생성
+		Tv tv = new Tv();
+		Tv tv2 = new Tv();
+		
+		// 3. 객체의 사용 == 객체의 속성 & 기능 사용
+		tv.power(); // tv가 가진 기능을 사용
+		tv.channel = 7;
+		tv.channelUp();
+		tv.color = "white"; // tv의 속성을 변경 --> tv의 색깔을 white으로 변경
+		
+		tv2.power(); // --> 꼭 참조변수가 필요
+		
+		System.out.println("현재 채널은 " + tv.channel + "입니다.");
+	}
+}
