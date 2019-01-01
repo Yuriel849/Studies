5f65c1ee3b12922278c560a3a7fc5e1caf4e904f
Yuriel849 <yuriel849sp@gmail.com>
2019-01-01 12:28:47 +0900

    create for 1 Jan. 2019

---
+++ HTML_CSS_JS/jQuery/js/slider_banner.js
@@ -0,0 +1,50 @@
+
+$(document).ready(function(){
+	var visual = $('#brandVisual > ul > li');
+	var button = $('#buttonList > li');
+
+	var current = 0;
+	var setIntervalId ;
+
+	button.on({click:function(){
+        var tg = $(this);
+		var i = tg.index();
+        
+        button.removeClass('on');
+		tg.addClass('on');
+		move(i);
+		}
+    });
+
+    $('#wrap').on({
+        mouseover:function(){
+		  clearInterval(setIntervalId);
+		},
+		mouseout:function(){
+            timer();
+        }
+    });
+
+	timer();
+
+	function timer(){
+		setIntervalId = setInterval(function(){
+			var n = current + 1 ;
+			if ( n == visual.length){
+				n = 0;
+			}
+			button.eq(n).trigger('click');
+		}, 1500);
+	}
+
+	function move(i){
+		if(current == i) return ;
+		
+		var currentEl = visual.eq(current);
+		var nextEl = visual.eq(i);
+
+		currentEl.css({left:0}).stop().animate({left:'-100%'});
+		nextEl.css({left:'100%'}).stop().animate({left:0});
+		current = i ;
+	}	
+});
