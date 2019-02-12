19e03b1db650a6cd24b6f7dda222b334032942b9
Yuriel849 <yuriel849sp@gmail.com>
2019-02-12 10:36:26 +0900

    delete unnecessary project

--- Spring_study/Studies_03/Chapter_01/src/main/java/org/xml/connector/ConnectionMaker.java
+++
@@ -1,8 +0,0 @@
-package org.xml.connector;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionMaker {
-	public Connection makeNewConnection() throws ClassNotFoundException, SQLException;
-}
